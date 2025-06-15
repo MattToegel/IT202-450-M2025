d3a88964069ebdff687a76ad5dbd79fe6e46b35b
MattToegel <matthew.toegel@njit.edu>
2025-06-15 19:57:31 -0400

    added user authentication without persistancce

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -27,7 +27,7 @@ if (isset($_POST["email"], $_POST["password"])) {
 
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
-   
+
     // TODO 3: validate/use
     $hasError = false;
 
@@ -45,7 +45,7 @@ if (isset($_POST["email"], $_POST["password"])) {
         echo "Password must not be empty<br>";
         $hasError = true;
     }
-    
+
     if (strlen($password) < 8) {
         echo "Password too short<br>";
         $hasError = true;
@@ -54,7 +54,28 @@ if (isset($_POST["email"], $_POST["password"])) {
     if (!$hasError) {
 
         // TODO 4: Check password and fetch user
-        
+        $db = getDB();
+        $stmt = $db->prepare("SELECT id, email, password from Users where email = :email");
+        try {
+            $r = $stmt->execute([":email" => $email]);
+            if ($r) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                if ($user) {
+                    $hash = $user["password"];
+                    unset($user["password"]);
+                    if (password_verify($password, $hash)) {
+                        echo "Welcome, $email!<br>";
+                    } else {
+                        echo "Invalid password<br>";
+                    }
+                } else {
+                    echo "Email not found<br>";
+                }
+            }
+        } catch (Exception $e) {
+            echo "There was an error logging in<br>"; // user-friendly message
+            error_log("Login Error: " . var_export($e, true)); // log the technical error for debugging
+        }
     }
 }
 ?>
